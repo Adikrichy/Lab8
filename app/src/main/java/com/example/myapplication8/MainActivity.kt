30da225d6aa7a26b86d93892131d3a63aaafa2ec
Adikrichy <adil.erzhanoc.70@gmail.com>
2025-04-08 09:56:20 +0500

    Init the MainActivity

--- app/src/main/java/com/example/myapplication8/MainActivity.kt
+++ app/src/main/java/com/example/myapplication8/MainActivity.kt
@@ -1,47 +1,48 @@
 package com.example.myapplication8
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication8.ui.theme.MyApplication8Theme
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var randomCharacterEditText: EditText
+    private lateinit var serviceIntent: Intent
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val data = intent?.getCharExtra("randomCharacter", '?')
+            randomCharacterEditText.setText(data?.toString() ?: "")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplication8Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+        randomCharacterEditText = findViewById(R.id.editText_randomCharacter)
+        serviceIntent = Intent(applicationContext, RandomCharacterService::class.java)
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_start -> startService(serviceIntent)
+            R.id.button_end -> {
+                stopService(serviceIntent)
+                randomCharacterEditText.text.clear()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(broadcastReceiver, IntentFilter("my.custom.action.tag.lab6"))
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplication8Theme {
-        Greeting("Android")
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
 }
